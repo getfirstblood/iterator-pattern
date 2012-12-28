8f0fe465ee0fe9de80792dec6aa4e378d779c833
gaoweiwei <getfirstblood@gmail.com>
2012-12-28 20:30:19 +0800

    Initial Commit

---
+++ iterator pattern/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iterator pattern
+//
+//  Created by gaoweiwei on 12-12-28.
+//  Copyright (c) 2012年 gaoweiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
